8fa3b76bc474390f4c80ecd98ddc74b3fc516f92
Vanessa Peixoto <vanessa@mbp-de-vanessa.home>
2024-11-07 09:57:25 +0100

    remove app.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./app/store";
-import App from "./App";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import EmployeeList from "./pages/EmployeeList/EmployeeList";
+import CreateEmployee from "./pages/CreateEmployee/CreateEmployee";
+import './main.css'
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -11,7 +12,7 @@ root.render(
   <Provider store={store}>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<CreateEmployee />} />
         <Route path="/employee-list" element={<EmployeeList />} />
       </Routes>
     </Router>

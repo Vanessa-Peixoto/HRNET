f241036a13ee778da3a9850e0e31b6aa84db67ec
Vanessa Peixoto <vanessa@mbp-de-vanessa.home>
2024-12-02 11:35:04 +0100

    add redux persist

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,13 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import formReducer from "../features/formSlice";
 import employeeReducer from "../features/employeeSlice";
 
-const store = configureStore({
-  reducer: {
-    form: formReducer,
-    employee: employeeReducer,
-  },
+//Combine reducers
+const rootReducer = combineReducers({
+  form: formReducer,
+  employee: employeeReducer,
 });
 
+//Config Redux Persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+//Reducer persist
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//Config the store with the persistent reducer
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./app/store";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import EmployeeList from "./pages/EmployeeList/EmployeeList";
 import CreateEmployee from "./pages/CreateEmployee/CreateEmployee";
@@ -10,11 +11,13 @@ const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <Router>
       <Routes>
         <Route path="/" element={<CreateEmployee />} />
         <Route path="/employee-list" element={<EmployeeList />} />
       </Routes>
     </Router>
+    </PersistGate>
   </Provider>
 );
